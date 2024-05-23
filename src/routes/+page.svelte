e735810ce120e756bc07a3111b91c018ae125093
Mars <mars@kuality.design>
2024-05-23 16:38:28 -0400

    Added TailwindCSS, PostCSS, autprefixer, app.css played with the routes, and added ShadCN

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev" target="_blank">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">Hello World.</h1>
